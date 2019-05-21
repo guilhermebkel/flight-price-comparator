c9a14e2b1fc79a894798bd3adf918f9323d1a349
Guilherme <guilhermebromonschenkel@gmail.com>
2019-05-20 22:06:01 -0300

    Now it is possible to select places and search interval outside the 'puppeteer.js' Script

--- src/scripts/test.js
+++ src/scripts/test.js
@@ -1,52 +1,58 @@
 const {ViajaNet, Milhas, PassagensPromo, Voopter} = require('./puppeteer')
 const fs = require('fs');
 
-const dataIda = ["18", "04", "2019"]
-const dataVolta = ["21", "04", "2019"]
+// Departure and Return date
+const dataIda = ["05", "07", "2019"];
+const dataVolta = ["07", "08", "2019"];
 
-/* Initializes all functions */
+// Origin and Destination places
+const origem = "BHZ";
+const destino = "VIX";
 
-setInterval(function(){
+// Defines the search interval in minutes
+const searchInterval = 1; 
 
-ViajaNet(dataIda, dataVolta).then((value) => {
-    console.log(value);
-    fs.writeFile("src/data/Viajanet-data.json", JSON.stringify(value), function(err) {
-        if(err) {
-            return console.log(err);
-        }
-        console.log("-> Viajanet price was saved! \n");
+// Makes the web scrapping during the defined interval
+setInterval(() => {
+
+    ViajaNet(dataIda, dataVolta, origem, destino).then((value) => {
+        console.log(value);
+        fs.appendFile("src/data/Viajanet-data.json", JSON.stringify(value), function(err) {
+            if(err) {
+                return console.log(err);
+            }
+            console.log("-> Viajanet price was saved! \n");
+        }); 
+    });
+
+    Milhas(dataIda, dataVolta, origem, destino).then((value) => {
+        console.log(value);
+        fs.appendFile("src/data/123Milhas-data.json", JSON.stringify(value), function(err) {
+            if(err) {
+                return console.log(err);
+            }
+            console.log("-> 123Milhas price was saved! \n");
+        }); 
+    });
+
+    PassagensPromo(dataIda, dataVolta, origem, destino).then((value) => {
+        console.log(value);
+        fs.appendFile("src/data/PassagensPromo-data.json", JSON.stringify(value), function(err) {
+            if(err) {
+                return console.log(err);
+            }
+            console.log("-> PassagensPromo price was saved! \n");
+        }); 
+    });
+
+    Voopter(dataIda, dataVolta, origem, destino).then((value) => {
+        console.log(value);
+        fs.appendFile("src/data/Voopter-data.json", JSON.stringify(value), function(err) {
+            if(err) {
+                return console.log(err);
+            }
+            console.log("-> Voopter price was saved! \n");
+        }); 
     }); 
-});
 
-Milhas(dataIda, dataVolta).then((value) => {
-    console.log(value);
-    fs.writeFile("src/data/123Milhas-data.json", JSON.stringify(value), function(err) {
-        if(err) {
-            return console.log(err);
-        }
-        console.log("-> 123Milhas price was saved! \n");
-    }); 
-});
-
-PassagensPromo(dataIda, dataVolta).then((value) => {
-    console.log(value);
-    fs.writeFile("src/data/PassagensPromo-data.json", JSON.stringify(value), function(err) {
-        if(err) {
-            return console.log(err);
-        }
-        console.log("-> PassagensPromo price was saved! \n");
-    }); 
-});
-
-Voopter(dataIda, dataVolta).then((value) => {
-    console.log(value);
-    fs.writeFile("src/data/Voopter-data.json", JSON.stringify(value), function(err) {
-        if(err) {
-            return console.log(err);
-        }
-        console.log("-> Voopter price was saved! \n");
-    }); 
-}); 
-
-}, 60000); //Updates the price every 60 seconds
-   
+}, 60000*searchInterval);
